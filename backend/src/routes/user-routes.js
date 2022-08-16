9674b3b4bf05176ff661894bf735561b05f7f69b
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-08-16 13:55:15 -0300

    User controllers dao

--- backend/src/routes/user-routes.js
+++ backend/src/routes/user-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/user-controller');
+const UserController = require('../controllers/user-controller-dao');
 
 router
     .get('/users', UserController.getUsers)
